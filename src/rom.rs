7c2310f94df4ed0df7ed6bd33398ead1e87118b7
smirror <23025996+smirror@users.noreply.github.com>
2024-05-12 22:56:32 +0900

    add: unit test for roms

--- src/rom.rs
+++ src/rom.rs
@@ -16,3 +16,24 @@ impl ROM {
         self.data.clone()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_rom_new() {
+        let data = vec![0; 0x100];
+        let rom = ROM::new(data.clone());
+        assert_eq!(rom.get_buf(), data);
+    }
+
+    #[test]
+    fn test_rom_read() {
+        let data = vec![0; 0x100];
+        let rom = ROM::new(data.clone());
+        for i in 0..0x100 {
+            assert_eq!(rom.read(i), data[i]);
+        }
+    }
+}
